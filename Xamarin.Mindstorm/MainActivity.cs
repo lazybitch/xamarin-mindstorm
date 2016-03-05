ecb939462c1a16775ab98d14f39d41af5c27f7be
Stadnik Piotr <stadnik.piotr@gmail.com>
2016-03-05 21:42:01 +0100

    Empty, cleaned up project.

---
+++ Xamarin.Mindstorm/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿namespace Xamarin.Mindstorm
+{
+    using Android.App;
+    using Android.OS;
+
+    [Activity(Label = "Xamarin.Mindstorm", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
