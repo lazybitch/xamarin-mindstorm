b9d3c531bfedb17312dbf458f292b6ea8d0125b4
Stadnik Piotr <stadnik.piotr@gmail.com>
2016-03-15 21:04:02 +0100

    Added motor message to command service.

--- Xamarin.Mindstorm/Infrastructure/MindstormCommandService.cs
+++ Xamarin.Mindstorm/Infrastructure/MindstormCommandService.cs
@@ -23,5 +23,54 @@ namespace Xamarin.Mindstorm.Infrastructure
 
             return message;
         }
+
+        public static MindstormMessage GetMotorMessage(int motor, int speed, int step)
+        {
+            if (speed > 100)
+            {
+                speed = 100;
+            }
+
+            if (speed < -100)
+            {
+                speed = -100;
+            }
+
+            var message = new MindstormMessage(14)
+            {
+                Payload =
+                {
+                    [0] = 12,
+                    [1] = 0,
+                    [2] = MindstormCommands.DirectCommandNoReply,
+                    [3] = MindstormCommands.SetOutputState,
+                    [4] = (byte) motor
+                }
+            };
+
+            if (speed == 0)
+            {
+                message.Payload[5] = 0;
+                message.Payload[6] = 0;
+                message.Payload[7] = 0;
+                message.Payload[8] = 0;
+                message.Payload[9] = 0;
+            }
+            else
+            {
+                message.Payload[5] = (byte) speed;
+                message.Payload[6] = MindstormCommands.MotorOn + MindstormCommands.Brake;
+                message.Payload[7] = MindstormCommands.RegulationModeMotorSpeed;
+                message.Payload[8] = 0x00;
+                message.Payload[9] = MindstormCommands.MotorRunStateRunning;
+            }
+
+            message.Payload[10] = (byte) step;
+            message.Payload[11] = (byte) (step >> 8);
+            message.Payload[12] = (byte) (step >> 16);
+            message.Payload[13] = (byte) (step >> 24);
+
+            return message;
+        }
     }
 }
