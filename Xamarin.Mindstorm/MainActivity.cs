87374654b2451ec5666a6884d342797d07daad27
Stadnik Piotr <stadnik.piotr@gmail.com>
2016-03-20 21:00:42 +0100

    Start, stop, speed controls.

--- Xamarin.Mindstorm/MainActivity.cs
+++ Xamarin.Mindstorm/MainActivity.cs
@@ -23,18 +23,50 @@
 
             buttonTestSound.Click += (s, e) =>
             {
-                var message1 = MindstormCommandService.GetToneMessage(3500, 1000);
+                var message = MindstormCommandService.GetToneMessage(3500, 1000);
 
-                communicator.WriteMessage(message1);
+                communicator.WriteMessage(message);
             };
 
             var buttonTestMotor = FindViewById<Button>(Resource.Id.ButtonTestMotor);
 
             buttonTestMotor.Click += (s, e) =>
             {
-                var message1 = MindstormCommandService.GetMotorMessage(MindstormComponents.MotorA, 100, 100);
+                var message = MindstormCommandService.GetMotorMessage(MindstormComponents.MotorA, 100, 100);
 
-                communicator.WriteMessage(message1);
+                communicator.WriteMessage(message);
+            };
+
+            var seekBarSpeed = FindViewById<SeekBar>(Resource.Id.SeekBarSpeed);
+
+            seekBarSpeed.ProgressChanged += (s, e) =>
+            {
+                var speed = e.Progress;
+                var message = MindstormCommandService.GetMotorMessage(MindstormComponents.MotorA, speed);
+
+                communicator.WriteMessage(message);
+            };
+
+            var buttonStartMotor = FindViewById<Button>(Resource.Id.ButtonStartMotor);
+
+            buttonStartMotor.Click += (s, e) =>
+            {
+                var speed = 50;
+                seekBarSpeed.Progress = speed;
+
+                var message = MindstormCommandService.GetMotorMessage(MindstormComponents.MotorA, speed);
+
+                communicator.WriteMessage(message);
+            };
+
+            var buttonStopMotor = FindViewById<Button>(Resource.Id.ButtonStopMotor);
+
+            buttonStopMotor.Click += (s, e) =>
+            {
+                seekBarSpeed.Progress = 0;
+                var message = MindstormCommandService.GetMotorMessage(MindstormComponents.MotorA, 0);
+
+                communicator.WriteMessage(message);
             };
         }
     }
