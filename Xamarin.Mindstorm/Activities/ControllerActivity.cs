f351e0c054fd1205662c084b277f4d8de3911036
Stadnik Piotr <stadnik.piotr@gmail.com>
2016-03-31 21:09:26 +0200

    Added button touch handlers on controller activity.

--- Xamarin.Mindstorm/Activities/ControllerActivity.cs
+++ Xamarin.Mindstorm/Activities/ControllerActivity.cs
@@ -2,6 +2,10 @@ namespace Xamarin.Mindstorm.Activities
 {
     using Android.App;
     using Android.OS;
+    using Android.Views;
+    using Android.Widget;
+    using Constants;
+    using Infrastructure;
 
     [Activity(Label = "Controller", Theme = "@android:style/Theme.Holo.Light")]
     public class ControllerActivity : Activity
@@ -11,6 +15,73 @@ namespace Xamarin.Mindstorm.Activities
             base.OnCreate(bundle);
 
             SetContentView(Resource.Layout.Controller);
+
+            var controllerService = new MindstormControllerService();
+
+            FindViewById<ImageButton>(Resource.Id.ButtonForward).Touch += (s, e) =>
+            {
+                switch (e.Event.Action)
+                {
+                    case MotionEventActions.Down:
+                        controllerService.ProcessMovement(MindstormMovement.Forward);
+                        break;
+                    case MotionEventActions.Up:
+                        controllerService.ProcessMovement(MindstormMovement.None);
+                        break;
+                }
+            };
+
+            FindViewById<ImageButton>(Resource.Id.ButtonForwardLeft).Touch += (s, e) =>
+            {
+                switch (e.Event.Action)
+                {
+                    case MotionEventActions.Down:
+                        controllerService.ProcessMovement(MindstormMovement.Forward | MindstormMovement.Left);
+                        break;
+                    case MotionEventActions.Up:
+                        controllerService.ProcessMovement(MindstormMovement.None);
+                        break;
+                }
+            };
+
+            FindViewById<ImageButton>(Resource.Id.ButtonLeft).Touch += (s, e) =>
+            {
+                switch (e.Event.Action)
+                {
+                    case MotionEventActions.Down:
+                        controllerService.ProcessMovement(MindstormMovement.Left);
+                        break;
+                    case MotionEventActions.Up:
+                        controllerService.ProcessMovement(MindstormMovement.None);
+                        break;
+                }
+            };
+
+            FindViewById<ImageButton>(Resource.Id.ButtonForwardRight).Touch += (s, e) =>
+            {
+                switch (e.Event.Action)
+                {
+                    case MotionEventActions.Down:
+                        controllerService.ProcessMovement(MindstormMovement.Forward | MindstormMovement.Right);
+                        break;
+                    case MotionEventActions.Up:
+                        controllerService.ProcessMovement(MindstormMovement.None);
+                        break;
+                }
+            };
+
+            FindViewById<ImageButton>(Resource.Id.ButtonRight).Touch += (s, e) =>
+            {
+                switch (e.Event.Action)
+                {
+                    case MotionEventActions.Down:
+                        controllerService.ProcessMovement(MindstormMovement.Right);
+                        break;
+                    case MotionEventActions.Up:
+                        controllerService.ProcessMovement(MindstormMovement.None);
+                        break;
+                }
+            };
         }
     }
 }
